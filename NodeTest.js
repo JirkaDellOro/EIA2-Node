f639bdd08346502f62bcf01438c0aed7ae7528bf
Jirka <del@hs-furtwangen.de>
2017-06-13 12:23:31 +0200

    Server installiert

--- NodeTest.js
+++ NodeTest.js
@@ -1,6 +1,20 @@
-console.log("Hallo");
-process.addListener("beforeExit", sayBye);
-function sayBye() {
-    console.log("Good bye");
+"use strict";
+console.log("Server starting");
+const Http = require("http");
+let port = process.env.PORT;
+if (port == undefined)
+    port = 8100;
+let server = Http.createServer();
+server.addListener("listening", handleListen);
+server.addListener("request", handleRequest);
+server.listen(port);
+function handleListen() {
+    console.log("Listening on port: " + port);
+}
+function handleRequest(_request, _response) {
+    console.log("Request received");
+    _response.setHeader("content-type", "text/html; charset=utf-8");
+    _response.write("Ich höre Stimmen!");
+    _response.end();
 }
 //# sourceMappingURL=NodeTest.js.map
--- NodeTest.ts
+++ NodeTest.ts
@@ -1,6 +1,23 @@
-console.log("Hallo");
-process.addListener("beforeExit", sayBye);
+console.log("Server starting");
 
-function sayBye(): void {
-    console.log("Good bye");
+import Http = require("http");
+
+let port: number = process.env.PORT;
+if (port == undefined)
+    port = 8100;
+
+let server: Http.Server = Http.createServer();
+server.addListener("listening", handleListen);
+server.addListener("request", handleRequest);
+server.listen(port);
+
+function handleListen(): void {
+    console.log("Listening on port: " + port);
+}
+
+function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): void {
+    console.log("Request received");
+    _response.setHeader("content-type", "text/html; charset=utf-8");
+    _response.write("Ich höre Stimmen!");
+    _response.end();
 }
